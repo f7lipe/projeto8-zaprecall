1ee5ffe23002df1892478e68ea9d7b325265bad1
= <f7lipe@me.com>
2022-03-19 20:45:43 -0300

    commit initial

---
+++ src/Launch.js
@@ -0,0 +1,12 @@
+export default function Launch() {
+    return (
+        <section className="launch d-y-center">
+
+            <img src="images/logo.png" alt=""/>
+                <p>ZapRecall</p>
+
+                <button >Iniciar Recall!</button>
+
+        </section>
+    )
+}
